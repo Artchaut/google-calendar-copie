a00279e383e65b3b5f7edc2c6e95aec6b3f7c61c
Artchaut <artichaut432@gmail.com>
2024-03-27 09:41:09 +0100

    Deno adapter

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -4,7 +4,7 @@ export const load = async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
 
 	if (!session) {
-		throw redirect(303, '/');
+		return { session: null, profile: null};
 	}
 
 	const { data: profile } = await supabase
