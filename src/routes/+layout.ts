839c83f9b42ea0d4ab2231ec867f31d76eab0c45
Artchaut <artichaut432@gmail.com>
2024-03-28 11:44:30 +0100

    Loading env variables regarding prod

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,13 +1,15 @@
-import { invalidate } from '$app/navigation';
+//import { invalidate } from '$app/navigation';
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
+	const env: string = import.meta.env.MODE ?? "DEV";
+	console.log(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 	const supabase = createSupabaseLoadClient({
-		supabaseUrl: PUBLIC_SUPABASE_URL,
-		supabaseKey: PUBLIC_SUPABASE_ANON_KEY,
+		supabaseUrl:  env === "PROD" ? import.meta.env.PUBLIC_SUPABASE_URL : PUBLIC_SUPABASE_URL,
+		supabaseKey: env == "PROD" ? import.meta.env.PUBLIC_SUPABASE_ANON_KEY : PUBLIC_SUPABASE_ANON_KEY,
 		event: { fetch },
 		serverSession: data.session
 	});
