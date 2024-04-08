6970e85a3d34bc201fb848a2ac524961195a8add
Gab <gabthuet@gmail.com>
2024-04-08 09:34:43 +0200

    WSH

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Notification from "$lib/components/Notification.svelte"	
+
+	let afficher = false
+	let c="Bouton"
+
+	
+	$:  afficher ? c = "Fermer" :  c = "Bouton"
+</script>
+
+
+<button on:click={
+	function () { 
+		afficher = !afficher 
+	}
+}>{c}</button>
+
+ 
+ {#if afficher}
+	<Notification alert_text={"Hello "} classname={"alert alert-info w-50" }/> 
+	
+ {/if}
